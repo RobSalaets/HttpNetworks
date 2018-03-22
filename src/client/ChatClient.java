0ca8c7aca2d896d2e9da8e3e2d82ad2c4e64de30
RubenPants <broekxruben@gmail.com>
2018-03-22 15:08:43 +0100

    cleaned client code

--- src/client/ChatClient.java
+++ src/client/ChatClient.java
@@ -18,20 +18,14 @@ public class ChatClient{
 	private PrintWriter logWriter;
 	private String currentHostHttp;
 
-	public ChatClient(){
-		// TODO
-		/*
-		try{
-			responseLog = new File("./headerLog.txt");
-			responseLog.createNewFile();
-			logWriter = new PrintWriter(responseLog, "UTF-8");
-			currentHostHttp = "";
-		}catch (IOException e){
-			e.printStackTrace();
-		}
-		*/
-	}
-
+	/*
+	 * Connect to a given server.
+	 * 
+	 * @param	host
+	 * 			The host server with whom you want to connect
+	 * @param	port
+	 * 			The port on which the connection is served
+	 */
 	public void connect(String host, int port){
 		try{
 			socket = new Socket(host, port);
@@ -43,6 +37,22 @@ public class ChatClient{
 		}
 	}
 
+	/*
+	 * This method will send the user client's command to the server
+	 * 
+	 * @param	command
+	 * 			GET, HEAD, PUT, or POST
+	 * @param	host
+	 * 			The host server
+	 * @param	resource
+	 * 			The path within the server
+	 * @param	httpNumber
+	 * 			HTTP/1.0 or HTTP/1.1
+	 * @param	content
+	 * 			The content the client wants to send to the host, in the case of a put or post
+	 * @param	closeHost
+	 * 			Determine if the user wants to close its connection with the host
+	 */
 	public void httpCommand(String command, String host, String resource, String httpNumber, String content, boolean closeHost){		
 		if(!currentHostHttp.isEmpty() && !currentHostHttp.equals(httpNumber))
 			System.out.println("Host uses other http version: " + currentHostHttp);
@@ -53,10 +63,12 @@ public class ChatClient{
 		System.out.println("Host: " + host);
 		out.println("User-Agent: " + "CNHttpChatclient/1.0");
 		System.out.println("User-Agent: " + "CNHttpChatclient/1.0");
+		
 		if(httpNumber.endsWith("1.1") && closeHost) {
 			out.println("Connection: close");
 			System.out.println("Connection: close");
 		}
+		
 		if(command.toLowerCase().equals("put") || command.toLowerCase().equals("post")) {
 			out.println("Content-Type: text/plain");
 			System.out.println("Content-Type: text/plain");
@@ -67,16 +79,21 @@ public class ChatClient{
 			out.println(content);
 			System.out.println(content);
 		}
-		else if (command.toLowerCase().equals("head")) {
-		}
-		out.println();
 		
+		out.println();
 		System.out.println();
 	}
 
+	/*
+	 * Wait for the resources from a html file, i.e. the embedded objects
+	 * 
+	 * @param	filename
+	 * 			The file where the html is saved
+	 * @return	Return false when the user has to wait, true when all the resources are fetched
+	 */
 	public boolean waitForResource(String filename){
-		// TODO: Make for every host its own header file
 		if (!filename.contains("/")) {
+			// Create headerLog-file
 			try {
 				responseLog = new File("./header_"+filename.substring(0, filename.length()-5)+".txt");
 				responseLog.createNewFile();
@@ -89,21 +106,16 @@ public class ChatClient{
 		
 		try{
 			String line = readLine();
-			// TODO: Infinity loop bij false, indien true lijkt alles wel te werken? 
-			// 	--> Wat is er fout bij true? Indien null dan is einde byte-array bereikt dus is er geen resource?
-			//  --> Wordt er niet al bij de inputStream op antwoord gewacht?
-			if(line == null)
-				return true;
-			// TODO
-			//System.out.println(line + " for resource: " + filename);
-			//System.out.println();
+			
+			if(line == null) return true;
+			
 			if(line.toLowerCase().startsWith("http/"))
 				currentHostHttp = line.split(" ")[0].trim();
+			
 			if(line.toLowerCase().contains("200 ok")) {
 				System.out.println(line);
 				File file = new File(filename);
-				if(filename.contains("/"))
-					file.getParentFile().mkdirs();
+				if(filename.contains("/")) file.getParentFile().mkdirs();
 				file.createNewFile();
 				FileOutputStream binWriter = new FileOutputStream(file);
 				PrintWriter writer = new PrintWriter(file, "UTF-8");
@@ -111,29 +123,36 @@ public class ChatClient{
 				StringBuffer headers = new StringBuffer();
 				boolean binaryContent = false;
 				int contentLength = -1;
+				
 				while(!line.isEmpty()){
-					// TODO
-					if (!filename.contains("/"))	// Do not save the headers of the embedded objects
+					// Do not save the headers of the embedded objects
+					if (!filename.contains("/"))
 						logWriter.println(line);
+					
 					headers.append(line + "\n");
 					if(line.toLowerCase().contains("content-type") && line.toLowerCase().contains("image"))
 						binaryContent = true;
-					if(line.toLowerCase().contains("content-length")) {
+					
+					if(line.toLowerCase().contains("content-length"))
 						contentLength = Integer.parseInt(line.split(":")[1].trim());
-					}
+					
 					line = readLine();
 				}
-				//logWriter.write(headers.toString() + "\n");
+				
 				byte[] buffer = new byte[contentLength];
 				int read = 0;
 				int toRead = contentLength;
+				
 				do {
 					read = in.read(buffer, 0, toRead);
+					
 					if(read == -1) break;
 					if(binaryContent) binWriter.write(buffer, 0, read);
 					else writer.write(new String(buffer, 0, read));
+					
 					toRead -= read;
 				}while(toRead > 0);
+				
 				writer.close();
 				binWriter.close();
 				return true;
@@ -143,16 +162,24 @@ public class ChatClient{
 					line = readLine();
 				}
 				System.out.println();
+				
 				return true;
 			}
 		}catch (IOException e){
 			e.printStackTrace();
 		}
+		
 		return false;
 	}
 	
+	/*
+	 * Wait for the response of the server
+	 * 
+	 * @return	Return true is the client has to wait, false otherwise
+	 */
 	public boolean waitForResponse(){
 		String line = readLine();
+		
 		if(line.toLowerCase().startsWith("http/")) {
 			currentHostHttp = line.split(" ")[0].trim();
 			while(!line.isEmpty()){
@@ -164,46 +191,64 @@ public class ChatClient{
 		return true;
 	}
 
+	/*
+	 * Read an input line byte for byte
+	 * 
+	 * @return	Return the character that corresponds with the byte that is read
+	 */
 	private String readLine(){
 		ByteOutputStream bo = new ByteOutputStream();
 		int character= -1;
+		
 		try{
 			do{
 				socket.setSoTimeout(2000);
 				character = in.read();
-				if(character == -1)
-					break;
+				
+				if(character == -1) break;
+				
 				else if(character == '\r'){
 					in.mark(2);
 					character = in.read();
-					if(character == '\n'){
-						break;
-					}
-					in.reset();
-				}else if(character == '\n')
-					break;
+					if(character == '\n') break;
+					in.reset();	
+				}
+				else if(character == '\n') break;
+				
 				else bo.write(character);
 			}while(character != -1);
 		}catch (IOException ignore){
+			// Socket timed out. Return null and the receiving method will handle the null-string
 		}
+		
 		String res = bo.toString();
 		bo.close();
+		
 		return character == -1 ? null : res;
 	}
 
+	/*
+	 * Close the connection with the host
+	 */
 	public void close(){
 		try{
-			socket.close();
-			in.close();
-			out.close();
-			if (logWriter != null)
-				logWriter.close();
+			if (socket != null) socket.close();
+			if (in != null) in.close();
+			if (out != null) out.close();
+			if (logWriter != null) logWriter.close();
 		}catch (IOException e){
 			e.printStackTrace();
 		}
 	}
 	
+	/*
+	 * Receive and store the header of the http request
+	 * 
+	 * @param	filename
+	 * 			The file where the html file is stored
+	 */
 	public boolean waitForHeader(String filename) {
+		// Create a headerLog-file
 		if (!filename.contains("/")) {
 			try {
 				responseLog = new File("./header_"+filename+".txt");
@@ -218,11 +263,11 @@ public class ChatClient{
 		String line = readLine();
 		if(line.toLowerCase().startsWith("http/"))
 			currentHostHttp = line.split(" ")[0].trim();
+		
 		if(line.toLowerCase().contains("200 ok")) {
 			StringBuffer headers = new StringBuffer();
 			while(!line.isEmpty()){
-				if (!filename.contains("/"))
-					logWriter.println(line);
+				if (!filename.contains("/")) logWriter.println(line);
 				headers.append(line + "\n");
 				line = readLine();
 			}
--- src/client/Main.java
+++ src/client/Main.java
@@ -19,27 +19,26 @@ public class Main{
 
 	public static void main(String[] args){
 		ChatClient client = new ChatClient();
+		
+		String command = args[0];
 		String[] urlData = getHostAndPath(args[1]);
 		String host = urlData[0];
 		String resource = urlData[1];
 		int port = Integer.parseInt(args[2]);
-		String command = args[0];
 		String httpNumber = args[3];
+		
 		Scanner terminalInput = new Scanner(System.in);
 
+		// Handle the given command
 		switch(command){
 			case "GET":
 				handleGet(client, host, port, command, resource, httpNumber);
 				break;
 			case "POST":
-				System.out.println("Enter content for your " + command + " request: ");
-				String contentPost = terminalInput.nextLine();
-				handlePutPost(client, host, port, command, resource, httpNumber, contentPost);
-				break;
 			case "PUT":
 				System.out.println("Enter content for your " + command + " request: ");
-				String contentPut = terminalInput.nextLine();
-				handlePutPost(client, host, port, command, resource, httpNumber, contentPut);
+				String content = terminalInput.nextLine();
+				handlePutPost(client, host, port, command, resource, httpNumber, content);
 				break;
 			case "HEAD":
 				handleHead(client, host, port, command, resource, httpNumber);
@@ -53,117 +52,185 @@ public class Main{
 		client.close();
 	}
 
+	/*
+	 * The GET-command will be handled in this method
+	 * 
+	 * @param	client
+	 * 			The client that is connected to the server
+	 * @param	host
+	 * 			The host-server
+	 * @param	port
+	 * 			The port used to connect to the host
+	 * @param	command
+	 * 			The type of command (GET, PUT, POST, or HEAD)
+	 * @param	resource
+	 * 			The path
+	 * @param	httpNumber
+	 * 			HTTP/1.0 or HTTP/1.1
+	 */
 	private static void handleGet(ChatClient client, String host, int port, String command, String resource, String httpNumber){
 		String htmlFileName = resource.endsWith("html") ? host + resource : resource.endsWith("/") ? host + resource.substring(0, resource.length() - 1) + ".html" : host + resource + ".html";
 		System.out.println("fileName: " + htmlFileName);
+		System.out.println();
 
 		try {
-			// Request html file
+			// Request HTML file
 			client.connect(host, port);
 			client.httpCommand(command, host, resource, httpNumber, null, false);
 		
-			while(!client.waitForResource(htmlFileName))
-				Thread.sleep(100);
+			while(!client.waitForResource(htmlFileName)) Thread.sleep(100);
+			
 			// Request embedded Images
 			Set<String> embeddedImages = lookupEmbedded(htmlFileName);
 			List<String> foreignHostResources = new ArrayList<String>();
-			int ii = 0;
+			int embeddedLocationIndex = 0;
 			for(String imageLocation : embeddedImages){
 				String imageResource = imageLocation.startsWith("/") ? imageLocation : "/" + imageLocation;
 				String imageHost = host;
+				
 				if(imageLocation.contains("//")){
 					String[] imageUrlData = getHostAndPath(imageLocation);
 					foreignHostResources.add(imageUrlData[0]);
 					foreignHostResources.add(imageUrlData[1]);
 					continue;
 				}
-				if(httpNumber.endsWith("1.0"))
-					client.connect(imageHost, port);
-				client.httpCommand("GET", imageHost, imageResource, httpNumber, null, ii == embeddedImages.size() - 1);
-				while(!client.waitForResource("." + imageResource))
-					Thread.sleep(100);				
-				ii++;
+				
+				if(httpNumber.endsWith("1.0")) client.connect(imageHost, port);
+				
+				client.httpCommand("GET", imageHost, imageResource, httpNumber, null, 
+						embeddedLocationIndex == embeddedImages.size() - 1);
+				
+				while(!client.waitForResource("." + imageResource)) Thread.sleep(100);				
+				
+				embeddedLocationIndex++;
 			}
+			
 			// Request images from other hosts
 			for(int i = 0; i < foreignHostResources.size(); i += 2){
 				client.connect(foreignHostResources.get(i), port);
-				client.httpCommand("GET", foreignHostResources.get(i), foreignHostResources.get(i + 1), httpNumber, null, i == foreignHostResources.size() - 2);
-				while(!client.waitForResource("." + foreignHostResources.get(i + 1)))
-					Thread.sleep(100);			
+				client.httpCommand("GET", foreignHostResources.get(i), foreignHostResources.get(i + 1), 
+						httpNumber, null, i == foreignHostResources.size() - 2);
+				
+				while(!client.waitForResource("." + foreignHostResources.get(i + 1))) Thread.sleep(100);			
 			}
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 	}
 
+	/*
+	 * The PUT- and POST-command will be handled in this method
+	 * 
+	 * @param	client
+	 * 			The client that is connected to the server
+	 * @param	host
+	 * 			The host-server
+	 * @param	port
+	 * 			The port used to connect to the host
+	 * @param	command
+	 * 			The type of command (GET, PUT, POST, or HEAD)
+	 * @param	resource
+	 * 			The path
+	 * @param	httpNumber
+	 * 			HTTP/1.0 or HTTP/1.1
+	 */
 	private static void handlePutPost(ChatClient client, String host, int port, String command, String resource, String httpNumber, String content){
 		try{
 			client.connect(host, port);
 			client.httpCommand(command, host, resource, httpNumber, content, true);
-			// TODO: Infinity loop with response
-			while(client.waitForResponse())
-				Thread.sleep(100);
+			
+			while(client.waitForResponse()) Thread.sleep(100);
 		}catch (InterruptedException e){
 			e.printStackTrace();
 		}
 	}
 	
+	/*
+	 * The HEAD-command will be handled in the method
+	 * 
+	 * @param	client
+	 * 			The client that is connected to the server
+	 * @param	host
+	 * 			The host-server
+	 * @param	port
+	 * 			The port used to connect to the host
+	 * @param	command
+	 * 			The type of command (GET, PUT, POST, or HEAD)
+	 * @param	resource
+	 * 			The path
+	 * @param	httpNumber
+	 * 			HTTP/1.0 or HTTP/1.1
+	 */
 	private static void handleHead(ChatClient client, String host, int port, String command, String resource, String httpNumber) {
 		client.connect(host, port);
 		client.httpCommand(command, host, resource, httpNumber, null, false);
-			try {
-				while(client.waitForHeader(host))
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		
+		try {
+			while(client.waitForHeader(host)) Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	/*
+	 * Fetch embedded object that were found in the HTML-file
+	 * 
+	 * @param	fileName
+	 * 			The name of the file where the HTML is stored
+	 */
 	private static Set<String> lookupEmbedded(String fileName){
 		String data = "";
+		
 		try{
 			data = new String(Files.readAllBytes(Paths.get(fileName)));
 		}catch (IOException e){
-			e.printStackTrace();
+			System.out.println("ERROR: No such file exists");
+			System.out.println(e.getMessage());
 		}
+		
 		Document doc = Jsoup.parse(data);
 		Elements images = doc.select("img[src]");
 		Set<String> imageLocations = new HashSet<>();
+		
 		for(int i = 0; i < images.size(); i++){
 			imageLocations.add(images.get(i).attr("src"));
-			System.out.println("IMAGE URL: " + images.get(i).attr("src"));
 		}
-		System.out.println("");
+		
 		return imageLocations;
 	}
 
+	/*
+	 * Determine the host and the path from of a given URI
+	 * 
+	 * @param	fullUri
+	 * 			The full URI received as an input-argument
+	 */
 	public static String[] getHostAndPath(String fullUri){
 		String[] result = new String[2];
+		
 		if(fullUri.contains("//"))
 			try{
 				result[0] = new URL(fullUri).getHost();
 				String path = new URL(fullUri).getPath();
-				if(path.length() > 0)
-					result[1] = path;
+				
+				if(path.length() > 0) result[1] = path;
 				else result[1] = "/";
+				
 				return result;
 			}catch (MalformedURLException e){
 				throw new IllegalArgumentException(fullUri);
 			}
 		else{
 			String[] temp = fullUri.split("/", 2);
+			
 			if(temp.length == 1 || temp[1].length() == 0){
 				result[0] = temp[0];
 				result[1] = "/";
+				
 				return result;
 			} else {
 				return fullUri.split("/", 2);
-			} 
-			/*
-			else
-				throw new IllegalArgumentException("No host");
-				*/
+			}
 		}
 	}
 }
